747ff73054fdf4fc316b4c2e5010a4a038a98f32
lopezzj1 <lopezzj1@uw.edu>
2015-11-11 13:22:36 -0800

    Initial Commit

---
+++ iQuiz2Tests/iQuiz2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  iQuiz2Tests.swift
+//  iQuiz2Tests
+//
+//  Created by Jill Lopez on 11/11/15.
+//  Copyright © 2015 Jill Lopez. All rights reserved.
+//
+
+import XCTest
+@testable import iQuiz2
+
+class iQuiz2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
